dbbe9660286e917a8c025f4920c78f347f1bdda6
Jacob Tersander <you@example.com>
2020-08-20 21:08:26 +0200

    more things

--- kimchi-store/src/App.js
+++ kimchi-store/src/App.js
@@ -1,27 +1,69 @@
-import React from "react";
-import { TextField, Button } from "@material-ui/core";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useReducer } from "react";
+import { makeStyles, TextField, Button } from "@material-ui/core";
 
-export const AuthContext = React.createContext();
+const useStyles = makeStyles((theme) => ({
+  main: {
+    background: "#F8ABBA",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+  },
+}));
+
 const initialState = {
   newProduct: "",
   products: [],
-};
-
-const handleInputChange = (event) => {
-  setData({
-    ...data,
-    [event.target.name]: event.target.value,
-  });
+  count: 0,
 };
 
 function App() {
-  const [data, setData] = React.useState(initialState);
+  const classes = useStyles();
+  function reducer(state, action) {
+    switch (action.type) {
+      case "newtext":
+        return { ...state, newProduct: action.payload };
+      case "increment":
+        return { ...state, count: state.count + 1 };
+      case "addproduct":
+        return {
+          ...state,
+          products: [...state.products, state.newProduct],
+          newProduct: "",
+        };
+      default:
+        throw new Error();
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const handleInputChange = (event) => {
+  //   setState({
+  //     ...state,
+  //     [event.target.name]: event.target.value,
+  //   });
+  // };
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    dispatch({ type: "newtext", payload: event.target.value });
+  };
+
+  console.log(state);
+  // const [data, setData] = React.useState(initialState);
+  // value={data.newProduct}
   return (
-    <div>
-      <TextField id="standard-basic" label="Standard" value={data.newProduct} />
-      <Button variant="outlined" color="primary">
+    <div className={classes.main}>
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        value={state.newProduct}
+        onChange={(e) => handleChange(e)}
+      />
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() => dispatch({ type: "addproduct" })}
+      >
         Primary
       </Button>
     </div>
